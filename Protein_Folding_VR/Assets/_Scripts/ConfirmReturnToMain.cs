b68fef238c143ad4e7a10605be3155a1b90446e5
lucasdf09 <ld.fabri@hotmail.com>
2020-02-23 10:33:24 -0300

    Save pre-keyboard

---
+++ Protein_Folding_VR/Assets/_Scripts/ConfirmReturnToMain.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ConfirmReturnToMain : MonoBehaviour
+{
+    private ModalPanel modal_panel;
+
+    //private UnityAction ok_action;
+    //private UnityAction cancel_action;
+
+    [SerializeField]
+    private GameObject parent_panel;
+
+    private void Awake()
+    {
+        modal_panel = ModalPanel.Instance();
+        //ok_action = new UnityAction(okFunction);
+        //cancel_action = new UnityAction(cancelFunction);
+    }
+
+    // Send to the Modal Panel to set up the Buttons and Functions to call
+    public void confirmReturnToMain()
+    {
+        // Calls the Confirm function with: message to be shown, Ok assigned function, Cancel assigned function
+        modal_panel.Confirm("All the unsaved progress will be lost. Do you want to go to Main Menu ?", okFunction, cancelFunction);
+    }
+
+    // These are wrapped into UnityActions
+    private void okFunction()
+    {
+        // Exit the game
+        gameObject.GetComponent<LoadSceneOnClick>().LoadByIndex(0);
+    }
+
+    private void cancelFunction()
+    {
+        // Return to the parent panel
+        parent_panel.SetActive(true);
+    }
+}
